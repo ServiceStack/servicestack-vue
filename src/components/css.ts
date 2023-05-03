c4bab7d138c4af19a0079097a9d89a25bbbe7fe3
Demis Bellot <demis.bellot@gmail.com>
2023-05-03 12:03:37 +0800

    include col-span-3 in tailwind

--- src/components/css.ts
+++ src/components/css.ts
@@ -97,3 +97,6 @@ export const grid = {
     theadCellClass: "px-6 py-4 text-left text-sm font-medium tracking-wider whitespace-nowrap",
     toolbarButtonClass: "inline-flex items-center px-2.5 py-1.5 border border-gray-300 dark:border-gray-700 shadow-sm text-sm font-medium rounded text-gray-700 dark:text-gray-300 bg-white dark:bg-black hover:bg-gray-50 dark:hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:ring-offset-black",
 }
+export const dummy = {
+    colspans: "col-span-3 sm:col-span-3"
+}
