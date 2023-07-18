ce31741fcd359a907d3ec01b379715b3812e0375
Darren Reid <darren@reidmail.org>
2023-07-18 12:28:02 +1000

    Handle use of `AuthSecret` provider. (#2)
    * Handle use of `AuthSecret` provider.
    
    * Update SignIn.vue
    
    fix whitespace
    
    ---------
    
    Co-authored-by: Demis Bellot <demis.bellot@gmail.com>

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -58,8 +58,8 @@
 
 <script setup lang="ts">
 import type { AppMetadata, AuthenticateResponse, MetaAuthProvider } from '@/types'
-import { computed, onMounted, ref } from 'vue'
-import { ApiResult, each, toPascalCase } from '@servicestack/client'
+import { computed, inject, onMounted, ref } from 'vue'
+import { ApiResult, each, JsonServiceClient, toPascalCase } from '@servicestack/client'
 import { useAuth } from '@/use/auth'
 import { useClient } from '@/use/client'
 import { useMetadata } from '@/use/metadata'
@@ -80,7 +80,8 @@ const emit = defineEmits<{
 }>()
 
 const { getMetadata, createDto } = useMetadata()
-const client = useClient() 
+const client = useClient()
+const serviceClient = inject<JsonServiceClient>('client')!
 const { signIn } = useAuth()
 
 const server = getMetadata({assert:true}) as AppMetadata
@@ -138,6 +139,10 @@ const errorSummary = computed(() => {
 
 async function submit() {
     modelValue.value.provider = authProvider.value.name
+    if (authProvider.value.name === 'authsecret') {
+        serviceClient.headers.set("authsecret",modelValue.value.authsecret)
+        modelValue.value = createDto("Authenticate")
+    }
     api.value = await client.api(modelValue.value)
     if (api.value.succeeded) {
         const response = api.value.response as AuthenticateResponse
