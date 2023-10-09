1ff7fd8e91e5a28ee55161d347a933957715059c
Darren Reid <darren@reidmail.org>
2023-10-09 18:05:19 +1100

    Fix resolving issue for `src` when running on CI.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,14 @@
 import Vue from '@vitejs/plugin-vue';
+import path from 'path'
 import {defineConfig} from "vite";
 
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src')
+        },
+    },
     plugins: [Vue()],
     // @ts-ignore
     test: {
