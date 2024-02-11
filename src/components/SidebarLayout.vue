55585e58f695447b4a18090fd8eae23400e70e10
Demis Bellot <demis.bellot@gmail.com>
2024-02-11 17:40:02 +0800

    Update SidebarLayout.vue

--- src/components/SidebarLayout.vue
+++ src/components/SidebarLayout.vue
@@ -9,12 +9,12 @@
           <div :class="['absolute left-full top-0 flex w-16 justify-center pt-5', transition3]">
             <button type="button" @click="hide" class="-m-2.5 p-2.5">
               <span class="sr-only">Close sidebar</span>
-              <svg class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+              <svg class="h-6 w-6 text-white dark:text-black" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
               </svg>
             </button>
           </div>
-          <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-2">
+          <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-white dark:bg-black px-6 pb-2">
             <slot></slot>
           </div>
         </div>
@@ -23,14 +23,14 @@
 
     <!-- Static sidebar for desktop -->
     <div class="hidden lg:fixed lg:inset-y-0 lg:z-10 lg:flex lg:w-72 lg:flex-col">
-      <div class="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
+      <div class="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 dark:border-gray-700 bg-white dark:bg-black px-6">
         <slot></slot>
       </div>
     </div>
 
     <!-- sticky titlebar for mobile -->
-    <div :class="['sticky top-0 flex items-center gap-x-6 bg-white px-4 py-4 shadow-sm sm:px-6 lg:hidden']">
-        <button type="button" @click="show" class="-m-2.5 p-2.5 text-gray-700 lg:hidden">
+    <div :class="['sticky top-0 flex items-center gap-x-6 bg-white dark:bg-black px-4 py-4 shadow-sm sm:px-6 lg:hidden']">
+        <button type="button" @click="show" class="-m-2.5 p-2.5 text-gray-700 dark:text-gray-200 lg:hidden">
             <span class="sr-only">Open sidebar</span>
             <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
               <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
