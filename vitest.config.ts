cb454805fbca0c124822146e712d905577658e90
Demis Bellot <demis.bellot@gmail.com>
2025-10-02 12:11:00 +0800

    use playright

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,20 +1,21 @@
-import Vue from '@vitejs/plugin-vue';
-import path from 'path'
-import {defineConfig} from "vite";
-
+import Vue from "@vitejs/plugin-vue";
+import path from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src')
-        },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    plugins: [Vue()],
-    // @ts-ignore
-    test: {
-        browser: {
-            enabled: true,
-            name: 'chrome', // browser name is required
-        },
-    }
-})
+  },
+  plugins: [Vue()],
+  // @ts-ignore
+  test: {
+    browser: {
+      provider: "playwright", // or 'webdriverio'
+      enabled: true,
+      // at least one instance is required
+      instances: [{ browser: "chromium" }],
+    },
+  },
+});
